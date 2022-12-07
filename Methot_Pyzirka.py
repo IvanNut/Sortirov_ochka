872a3d8b2884b3fcfd605bbf91abd66e3a54b711
Neuer <Neuer_xxxx@mail.ru>
2022-12-07 19:53:59 +0300

    methot pyz

---
+++ Methot_Pyzirka.py
@@ -0,0 +1,12 @@
+from random import randint
+
+
+def bubble(array):
+    for i in range(N - 1):
+        for j in range(N - i - 1):
+            if array[j] > array[j + 1]:
+                buff = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = buff
+
+
